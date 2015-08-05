7c9b16308e0289614125fb25aac9b58d383c60f2
Djxy <samuel.m.lavoie@gmail.com>
2015-08-05 09:44:48 -0400

    .

--- djxy/models/component/Component.java
+++ djxy/models/component/Component.java
@@ -1,5 +1,7 @@
 /*
  * Copyright (C) Samuel Marchildon-Lavoie - All Rights Reserved
+ *
+ * License: https://github.com/djxy/MinecraftGUI-Plugin/blob/master/License.txt
  */
 
 package djxy.models.component;
--- djxy/models/component/ComponentType.java
+++ djxy/models/component/ComponentType.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright (C) Samuel Marchildon-Lavoie - All Rights Reserved
+ *
+ * License: https://github.com/djxy/MinecraftGUI-Plugin/blob/master/License.txt
+ */
+
 package djxy.models.component;
 
 public enum ComponentType{
